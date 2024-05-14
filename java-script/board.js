2ade494e78fb546e39ad492fffe8af358726677d
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-05-14 18:02:49 +0200

    task-container added

--- java-script/board.js
+++ java-script/board.js
@@ -1,3 +1,21 @@
+let currentTask = 0;
+const totalSubtasks = 2;
+
+function updateProgressBar() {
+    let percent = (currentTask / totalSubtasks) * 100; // Verwendung von totalSubtasks
+    percent = Math.round(percent);
+    document.getElementById('progress-bar').style.width = percent + "%";
+    document.getElementById('progress-count').innerHTML = (currentTask + 1) + "/" + totalSubtasks + " Subtasks"; // Aktualisierung der Subtask-Anzeige
+}
+
+
+
+
+
+
+
+
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
