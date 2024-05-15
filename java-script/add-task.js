73ef48d0b064dc628a0826cca52e90054294985d
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-05-15 16:31:08 +0200

    add task assigned to area added

--- java-script/add-task.js
+++ java-script/add-task.js
@@ -15,3 +15,27 @@ async function includeHTML() {
         }
     }
 }
+
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "block") {
+      dropdownMenu.style.display = "none";
+    } else {
+      dropdownMenu.style.display = "block";
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+      var dropdowns = document.getElementsByClassName("dropdown-menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
+  
