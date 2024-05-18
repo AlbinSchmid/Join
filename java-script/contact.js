0fed8c27a1f5e9d5568e667409872911f2b7a488
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-05-19 00:44:27 +0200

    contact.js - add contact

--- java-script/contact.js
+++ java-script/contact.js
@@ -13,10 +13,97 @@ async function includeHTML() {
 }
 
 
-// ------------- EDIT ICONS
+let allContacts = [];
+
+function init() {
+    loadAllContacts();
+}
+
+function addContact() {
+    let name = document.getElementById('name');
+    let mail = document.getElementById('mail');
+    let phonenumber = document.getElementById('phonenumber');
+    let contact = {
+        'color': getColor(),
+        'initials': getInitials(name),
+        'name': name.value,
+        'mail': mail.value,
+        'phonenumber': phonenumber.value,
+
+    }
+    allContacts.push(contact);
+    name.value = '';
+    mail.value = '';
+    phonenumber.value = '';
+    saveAllContacts()
+    loadAllContacts()
+}
+
+
+function getColor() {
+    const bgColors = ['#ff7a00', '#9327ff', '#6e52ff', '#fC71ff', '#ffbb2b', '#1fd7c1'];
+    const randomColor = bgColors[Math.floor(bgColors.length * Math.random())];
+    return randomColor;
+}
+
+function getInitials(name) {
+
+    };
+
+    
+function saveAllContacts() {
+   let allContactsAsString = JSON.stringify(allContacts);
+   localStorage.setItem('allContacts', allContactsAsString); 
+}
+
+
+function loadAllContacts() {
+    let allContactsAsString = localStorage.getItem('allContacts');
+    allContacts = JSON.parse(allContactsAsString);
+    console.log(allContacts)
+}
+
+
+
+
+
+
+
+// ------------- ENABLE/DISABLE CONTAINER ------------- //
+
+
+function showCreateNewContact() {
+    document.getElementById('addNewContact').classList.toggle('d-none');
+}
+
+
+function createNewContact() {
+    document.getElementById('addNewContact').classList.toggle('d-none');
+    document.getElementById('btnContactCreated').classList.toggle('d-none');
+    setTimeout(closeBtnSuccesfully, 1500);
+    addContact();
+}
+
+
+function closeBtnSuccesfully() {
+    document.getElementById('btnContactCreated').classList.add('d-none')
+}
+
+
+function showEditContact() {
+    document.getElementById('editContact').classList.toggle('d-none');
+}
+
+
+function showContact() {
+    document.getElementById('contactData').classList.toggle('d-none');
+}
+
+
+// ------------- EDIT ICONS ------------- //
 
 
 function changeEditIcon() {
     document.getElementById('editIcon').setAttribute('src', './assets/img/icons/contact/edit_blue.png');
-    
+
 }
