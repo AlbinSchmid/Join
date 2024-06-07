97d7fa792c9918ec3af9d3199815b9259b0bfe6e
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-06-07 20:35:23 +0200

    sign up update

--- java-script/login-signup.js
+++ java-script/login-signup.js
@@ -1,14 +1,7 @@
 let userList = [];
 
 
-function closeBtnSignUpSuccesfully(event) {
-    event.preventDefault();
-    addUser();
-};
-
-
 async function addUser() {
-    let name = document.getElementById('inputSignUpName');
     let mail = document.getElementById('inputSignUpMail');
     let password = document.getElementById('inputSignUpPassword1');
     let password2 = document.getElementById('inputSignUpPassword2');
@@ -18,39 +11,42 @@ async function addUser() {
         checkbox.style.border = "2px solid red";
         return;
     }
-    let user = {
-        'name': name.value,
-        'initials': getInitials(name.value),
-        'password': password.value,
-        'password2': password2.value,
-        'mail': mail.value,
+    if (!(await isEmailUnique(mail.value))) {
+        document.getElementById('passwordIncorrect').innerText = "This email is already registered";
+        mail.style.border = "2px solid red";
+        return;
     }
-    if (passwordCheck(user, password, password2)) {
-    }
-    else {
-        console.error('password incorrect');
+    let user = createUserObject();
+    if (await passwordCheck(user, password, password2)) {
     }
 };
+
+
+function createUserObject() {
+    let name = document.getElementById('inputSignUpName').value;
+    let mail = document.getElementById('inputSignUpMail').value;
+    let password = document.getElementById('inputSignUpPassword1').value;
+    return {
+        'name': name,
+        'initials': getInitials(name),
+        'password': password,
+        'mail': mail,
+    };
+}
+
+
+async function isEmailUnique(email) {
+    await loadUser();
+    return !userList.some(user => user.mail === email);
+}
+
 
 async function passwordCheck(user, password, password2) {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
-
-    if (passwordValue === "" || password2Value === "") {
-        let passwordIncorrect = document.getElementById('passwordIncorrect');
-        passwordIncorrect.innerHTML = "Passwords cannot be empty";
-        password.style.border = "2px solid red";
-        password2.style.border = "2px solid red";
-        return false;
+    if (await isPasswordEmpty(passwordValue, password2Value)) {
     }
-
-    if (passwordValue === password2Value) {
-        document.getElementById('bgSignupSuccesfully').classList.remove('d-none');
-        await postData('/users', user);
-        setTimeout(function () {
-            window.location.href = "./log-in.html";
-        }, 1500);
-        return true;
+    if (await isPasswordEven(user,passwordValue, password2Value)) {
     } else {
         let passwordIncorrect = document.getElementById('passwordIncorrect');
         passwordIncorrect.innerHTML = "Ups! your passwords don't match";
@@ -60,10 +56,30 @@ async function passwordCheck(user, password, password2) {
 }
 
 
+async function isPasswordEven(user, passwordValue, password2Value) {
+    if (passwordValue === password2Value) {
+        document.getElementById('bgSignupSuccesfully').classList.remove('d-none');
+        await postData('/users', user);
+        setTimeout(function () {
+            window.location.href = "./log-in.html";
+        }, 1500);
+        return true;
+    }
+}
+
+async function isPasswordEmpty(passwordValue, password2Value) {
+    if (passwordValue === "" || password2Value === "") {
+        let passwordIncorrect = document.getElementById('passwordIncorrect');
+        passwordIncorrect.innerHTML = "Passwords cannot be empty";
+        password2.style.border = "2px solid red";
+        return false;
+    }
+}
+
 async function loadUser() {
     userList = [];
-    let users = await getData('users'); 
-    let userIDs = Object.keys(users || []);    
+    let users = await getData('users');
+    let userIDs = Object.keys(users || []);
     for (let i = 0; i < userIDs.length; i++) {
         let userID = userIDs[i];
         let user = users[userID];
@@ -76,7 +92,7 @@ async function loadUser() {
 function login() {
     let mail = document.getElementById('inputLoginMail');
     let password = document.getElementById('inputLoginPassword');
-    let user = userList.find( u => u.mail == mail.value && u.password == password.value);
+    let user = userList.find(u => u.mail == mail.value && u.password == password.value);
     if (user) {
         let userAsText = JSON.stringify(user);
         localStorage.setItem('user', userAsText);
@@ -106,3 +122,9 @@ function getInitials(name) {
         .map(word => word[0].toUpperCase())  // Nimmt den ersten Buchstaben jedes Wortes und wandelt ihn in Großbuchstaben um
         .join("");                         // Verbindet die Buchstaben zu einer Zeichenkette
 }
+
+
+function closeBtnSignUpSuccesfully(event) {
+    event.preventDefault();
+    addUser();
+};
