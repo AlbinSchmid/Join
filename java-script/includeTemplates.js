c67cbb7b6821cc69f3dff58d3cd3221af5eab935
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-06-10 22:34:35 +0200

    Add activeLink Sidebar

--- java-script/includeTemplates.js
+++ java-script/includeTemplates.js
@@ -1,5 +1,5 @@
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
+    let includeElements = document.querySelectorAll("[w3-include-html]");
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
         file = element.getAttribute("w3-include-html"); // "includes/header.html"
@@ -7,25 +7,42 @@ async function includeHTML() {
         if (resp.ok) {
             element.innerHTML = await resp.text();
         } else {
-            element.innerHTML = 'Page not found';
+            element.innerHTML = "Page not found";
         }
     }
 }
 
-(async function(){
+(async function () {
     await includeHTML();
     showUser();
-})()
+    linkActive();
+})();
+
+
+function linkActive() {
+    const links = {
+        "/summary.html": "link-summary",
+        "/add-task.html": "link-task",
+        "/board.html": "link-board",
+        "/contact.html": "link-contact",
+    };
+    const pathname = window.location.pathname;
+    const id = links[pathname];
+    if (!id) {
+        console.error("The link doesn't exist", pathname);
+        return;
+    }
+    document.getElementById(id).classList.toggle("active-link");
+}
 
 
 function showUser() {
-    let userInitials = document.getElementById('userInitials');
+    let userInitials = document.getElementById("userInitials");
     if (!userInitials) {
-        console.error('Can not find container userInitials');
-        return
+        console.error("Can not find container userInitials");
+        return;
     }
-    let userAsText = localStorage.getItem('user');
+    let userAsText = localStorage.getItem("user");
     let user = JSON.parse(userAsText);
     userInitials.innerHTML = `<div>${user.initials}</div>`;
 }
-
